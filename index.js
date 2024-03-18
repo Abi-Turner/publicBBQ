41ee0fd88b80a4cf954bdf77b6c92e6d0f559ab5
Abi Turner <turner.abi@gmail.com>
2024-03-18 14:16:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+const API_URL = "https://data.melbourne.vic.gov.au/api/explore/v2.1/catalog/datasets/public-barbecues/records?limit=63"
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    const result = response.data.results;
+    res.render("index.ejs", { result });
+    console.log(result)
+  } catch (error) {
+    res.render("index.ejs", { error: error.message })
+    console.log(error.message)
+  }
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}`)
+});
